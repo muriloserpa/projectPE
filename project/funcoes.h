11a3e2cc7b7cd8646516b653a18da5397191ae65
muriloserpa <56055542+muriloserpa@users.noreply.github.com>
2022-03-25 19:00:29 -0300

    Add files via upload

---
+++ project/funcoes.h
@@ -0,0 +1,19 @@
+#ifndef FUNCOES_H_INCLUDED
+#define FUNCOES_H_INCLUDED
+
+#define MAX 10
+
+//TADs
+
+//var globais
+//int i=10;
+
+char *data;
+char code[4];
+float bid;
+
+
+
+void makeRequest();
+float conversion();
+#endif /* FUNCOES_H_INCLUDED */
---
+++ project/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lib.h"
+
+
+int main (){
+    float value;
+    printf("Para qual moeda deseja converter: ");
+    scanf("%s",code);
+	makeRequest();
+	printf("Qual o valor em BRL: ");
+    scanf("%f",&value);
+    value = value * bid;
+    printf("Valor em %s: %f",code,value);
+
+    return 0;
+}
